0ffcfac7974b52b6f19262217c3fe3b16113558f
syukrontanzilah <syukrontanzilah@gmail.com>
2024-06-23 19:00:36 +0700

    add search

--- src/App.js
+++ src/App.js
@@ -88,8 +88,15 @@ const App = () => {
   return (
       <div className='w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center px-4 lg:px-0'>
         {/* form */}
-        <form>
-
+        <form className='h-16 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-8'>
+          <div className='h-full relative flex items-center justify-between p-2'>
+            <input 
+            className='flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full'
+            type='text' placeholder='City or country'/>
+            <button className='bg-[#1ab8ed] hover:bg-[#15abdd] w-20 h-12 rounded-full flex justify-center items-center transition'>
+              <IoMdSearch className='text-2xl text-white'/>
+            </button>
+          </div>
         </form>
         {/* card */}
         <div className='w-full max-w-[450px] bg-black/20 min-h-[584px] text-white backdrop-blur-[32px] rounded-[32px] py-12 px-6'>
@@ -143,6 +150,24 @@ const App = () => {
                   <div className='flex ml-2'>{parseInt(data.main.feels_like)}
                     <TbTemperatureCelsius/>
                   </div>
+                </div>
+              </div>
+            </div>
+            <div className='flex justify-between'>
+              <div className='flex items-center gap-x-2'>
+                {/* icon */}
+                <div className='text-[20px]'><BsWater/></div>
+                <div>
+                  Humidity <span className='ml-2'>{data.main.humidity}%</span>
+                </div>
+              </div>
+              <div className='flex items-center gap-x-2'>
+                {/* icon */}
+                <div className='text-[20px]'>
+                  <BsWind/>
+                </div>
+                <div className=''>
+                  Wind <span className='ml-2'>{data.wind.speed}m/s</span>
                 </div>
               </div>
             </div>
