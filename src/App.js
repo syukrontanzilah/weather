4eed540ae59996d12f24ee0b1a5b47b4d4491bf4
syukrontanzilah <syukrontanzilah@gmail.com>
2024-06-23 19:38:56 +0700

    add input change

--- src/App.js
+++ src/App.js
@@ -26,7 +26,30 @@ const APIKey = 'befbe1072b7242f25bc785584e508a60'
 const App = () => {
   const [data, setData] = useState(null);
   const [location, setLocation] = useState('Bandung');
+  const [inputValue, setInputValue] = useState('');
+  const [animate, setAnimate] = useState(false);
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIKey}`;
+
+  const handleInput = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('input', inputValue)
+    if(inputValue !== ''){
+      setLocation(inputValue)
+    }
+    const input = document.querySelector('input');
+    if(input.value === ''){
+      setAnimate(true);
+      setTimeout(() => {
+        setAnimate(false);
+      }, 500);
+    }
+    input.value = '';
+  }
+
 
   const getData = async () => {
     try{
@@ -88,12 +111,15 @@ const App = () => {
   return (
       <div className='w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center px-4 lg:px-0'>
         {/* form */}
-        <form className='h-16 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-8'>
+        <form className={` ${animate? 'animate-shake' : 'animate-none'} h-16 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-8`}>
           <div className='h-full relative flex items-center justify-between p-2'>
             <input 
+            onChange={(e)=> handleInput(e)}
             className='flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full'
             type='text' placeholder='City or country'/>
-            <button className='bg-[#1ab8ed] hover:bg-[#15abdd] w-20 h-12 rounded-full flex justify-center items-center transition'>
+            <button
+            onClick={(e)=> handleSubmit(e)}
+            className='bg-[#1ab8ed] hover:bg-[#15abdd] w-20 h-12 rounded-full flex justify-center items-center transition'>
               <IoMdSearch className='text-2xl text-white'/>
             </button>
           </div>
